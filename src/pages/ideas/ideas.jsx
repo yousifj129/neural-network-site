50e2173635f5d40640910f7a72175afaf9f95550
yousif <138237408+yousifj129@users.noreply.github.com>
2024-10-30 20:37:58 +0300

    Update ideas.jsx

--- src/pages/ideas/ideas.jsx
+++ src/pages/ideas/ideas.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { IDEAS } from '../../ideas';
+import React from "react";
+import { IDEAS } from "../../ideas";
 export const Ideas = () => {
-    return (
-        <div>
-            <h1>random things page</h1>
-            <div className='ideas'> {IDEAS.map(idea => <div key={idea.id}><a href={idea.link}>{idea.title}</a> <p>{idea.description}</p></div>)}</div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>random things page</h1>
+      <div className="ideas">
+        {IDEAS.map((idea) => (
+          <div key={idea.id}>
+            <a href={idea.link}>{idea.title}</a> <p>{idea.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
